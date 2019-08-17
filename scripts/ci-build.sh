9f99dfcd559fc2f7497854f9297e1a64059e7604
sun <1184550842@qq.com>
2019-08-17 14:50:48 +0800

    modify travis script

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
 
-hugo --theme=hyde --baseUrl=https://tsingland.github.io
+
+# git config
+function genSite() {
+    hugo --theme=hyde --baseUrl=https://tsingland.github.io/
+}
+
+
+function gitConfig() {
+    git config --global user.email "1184550842@qq.com"
+    git config --global user.name "sunlin"
+}
+
+# commit tsingland
+function commit() {
+
+    cd public
+    git init
+    git add .
+    git commit -m "Update by travis"
+
+    git push -f "https://${GH_TOKEN}@${GH_REF}" master:master
+}
+
+function main() {
+    genSite
+    gitConfig
+    commit
+}
+
+main
